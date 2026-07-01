91c9fd2c359aa1ba5b8f06dab16868aee7386261
allan <yulun.wu1220@gmail.com>
2026-07-01 01:11:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import autoencoder
-import train 
-import loss 
+import train
+import loss
 import vae_dataset_loader
 
-import torch 
+import torch
 from pathlib import Path
 
 from torchvision import transforms
 from sklearn.model_selection import train_test_split
-import torchinfo 
+import torchinfo
 
 torch.cuda.manual_seed(10)
+torch.manual_seed(10)
 
 # define data path
 DATA_PATH = Path("train_autoencoder")
@@ -23,13 +24,20 @@ BATCH_SIZE = 8
 
 CHANNEL_LIST = [1, 128, 256, 512]
 LATENT_DIMS = 8
-ATTN_FLAGS = [False, False, True ]
+ATTN_FLAGS = [False, False, True]
 
 EPOCHS = 100
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-vae_dataset = vae_dataset_loader.load_vae_dataset(input_dir = DATA_PATH, transform = transform, n_jobs = NUM_WORKERS, n_patches = N_PATCHES, patch_size = PATCH_SIZE, cache_size = CACHE_SIZE)
+vae_dataset = vae_dataset_loader.load_vae_dataset(
+    input_dir=DATA_PATH,
+    transform=transform,
+    n_jobs=NUM_WORKERS,
+    n_patches=N_PATCHES,
+    patch_size=PATCH_SIZE,
+    cache_size=CACHE_SIZE,
+)
 
 print(len(vae_dataset))
 indices = list(range(len(vae_dataset)))
@@ -53,22 +61,25 @@ test_dataloader = torch.utils.data.DataLoader(
 )
 print("Done!")
 
-vae = autoencoder.VAE(channel_list= CHANNEL_LIST, latent_dim = LATENT_DIMS, attn_flag= ATTN_FLAGS, silu = True, int_res = True).to(device)
-discriminator = loss.snPATCH_discriminator(in_channel = 1 , base_ch = 64, n_layers = 3).to(device)
-
-torchinfo.summary(vae)
-torchinfo.summary(discriminator)
-
-loss_fn = loss.VAE_loss(
-    l1_w = 1.0,
-    kl_w = 1.0,
-    adv_w = 0.5, 
-    adv_start_step = 10000
+vae = autoencoder.VAE(
+    channel_list=CHANNEL_LIST,
+    latent_dim=LATENT_DIMS,
+    attn_flag=ATTN_FLAGS,
+    silu=True,
+    int_res=True,
+).to(device)
+discriminator = loss.snPATCH_discriminator(in_channel=1, base_ch=64, n_layers=3).to(
+    device
 )
+
+torchinfo.summary(vae, input_size=(BATCH_SIZE, 1, PATCH_SIZE, PATCH_SIZE))
+torchinfo.summary(discriminator, input_size=(BATCH_SIZE, 1, PATCH_SIZE, PATCH_SIZE))
+
+loss_fn = loss.VAE_loss(l1_w=1.0, kl_w=1.0, adv_w=0.5, adv_start_step=10000)
 
 vae_opt = torch.optim.AdamW(
     vae.parameters(),
-    lr=5.34e-04,
+    lr=1.34e-04,
     betas=(0.516, 0.972),
     weight_decay=9.38e-06,
     eps=9.69e-09,
@@ -76,25 +87,33 @@ vae_opt = torch.optim.AdamW(
 
 disc_opt = torch.optim.AdamW(
     discriminator.parameters(),
-    lr = 5.34e-04,
+    lr=1.34e-04,
     betas=(0.512, 0.972),
     weight_decay=9.38e-06,
     eps=9.69e-09,
 )
 
-steps_per_epoch = len(train_dataloader)  
-total_steps     = steps_per_epoch * EPOCHS
+steps_per_epoch = len(train_dataloader)
+total_steps = steps_per_epoch * EPOCHS
 
-kl_start = int(0.05 * total_steps) 
-kl_end   = int(0.40 * total_steps) 
+kl_start = int(0.05 * total_steps)
+kl_end = int(0.40 * total_steps)
 
 kl_annealing_scheduler = train.KL_cosine_annealing(start_step=kl_start, end_step=kl_end)
 
-result = train.train_vae(vae = vae, discriminator = discriminator,
-                        vae_opt = vae_opt, disc_opt = disc_opt,
-                        vae_loss = loss_fn, train_dataloader = train_dataloader,
-                        test_dataloader = test_dataloader,epochs = EPOCHS, vae_lr_scheduler= None,
-                        disc_lr_scheduler= None, model_name = "VAE_run_1", 
-                        kl_annealing_scheduler= kl_annealing_scheduler,
-                        device = device
-                        )
+result = train.train_vae(
+    vae=vae,
+    discriminator=discriminator,
+    vae_opt=vae_opt,
+    disc_opt=disc_opt,
+    vae_loss=loss_fn,
+    train_dataloader=train_dataloader,
+    test_dataloader=test_dataloader,
+    epochs=EPOCHS,
+    vae_lr_scheduler=None,
+    disc_lr_scheduler=None,
+    model_name="VAE_run_1",
+    kl_annealing_scheduler=kl_annealing_scheduler,
+    device=device,
+)
+
--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from alive_progress import alive_bar
 from pathlib import Path
 import pickle as pypickle
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 
 class KL_cosine_annealing:
@@ -38,6 +38,7 @@ def train_step(
     kl_scheduler: KL_cosine_annealing,
     step,
     device,
+    scaler,
 ):
 
     total_disc_loss, total_vae_loss = 0.0, 0.0
@@ -48,41 +49,47 @@ def train_step(
 
         X = X.to(device)
 
-        reconstruct, mu, log_var = vae(X)
+        with torch.autocast(device_type=device):
+            reconstruct, mu, log_var = vae(X)
 
         # train discriminator
         disc_opt.zero_grad()
 
         disc_recon = reconstruct.detach()
 
-        fake_score = discriminator(disc_recon)
-        real_score = discriminator(X)
+        with torch.autocast(device_type=device):
+            fake_score = discriminator(disc_recon)
+            real_score = discriminator(X)
 
-        loss_real, loss_fake = loss.hinge_discriminator(real_score, fake_score)
-        disc_loss = loss_real + loss_fake
+            loss_real, loss_fake = loss.hinge_discriminator(real_score, fake_score)
+            disc_loss = loss_real + loss_fake
 
-        disc_loss.backward()
+        scaler.scale(disc_loss).backward()
+        scaler.step(disc_opt)
 
-        disc_opt.step()
+        # disc_opt.step()
 
         # train vae
         vae_opt.zero_grad()
 
         kl_w = kl_scheduler.get_value(current_step=step)
 
-        vae_loss, loss_dict = loss_fn(
-            recon=reconstruct,
-            target=X,
-            mu=mu,
-            log_var=log_var,
-            disc=discriminator,
-            global_step=step,
-            kl_weight=kl_w,
-        )
+        with torch.autocast(device_type=device):
+            vae_loss, loss_dict = loss_fn(
+                recon=reconstruct,
+                target=X,
+                mu=mu,
+                log_var=log_var,
+                disc=discriminator,
+                global_step=step,
+                kl_weight=kl_w,
+            )
 
-        vae_loss.backward()
+        scaler.scale(vae_loss).backward()
 
-        vae_opt.step()
+        scaler.step(vae_opt)
+
+        # vae_opt.step()
 
         total_disc_loss += disc_loss.cpu().item()
         total_vae_loss += vae_loss.cpu().item()
@@ -99,6 +106,8 @@ def train_step(
 
         avg_disc_loss = total_disc_loss / len(train_dataloader)
         avg_vae_loss = total_vae_loss / len(train_dataloader)
+
+        scaler.update()
 
     return avg_vae_loss, avg_disc_loss, step
 
@@ -145,7 +154,7 @@ def test_step(vae, discriminator, test_dataloader, loss_fn, kl_scheduler, step, 
                     f"test step currently at : {i} / {len(test_dataloader)} \n"
                     f"avg_vae_loss : {total_vae_loss / max(i, 1)}"
                     f"loss_dict : {loss_dict} \n",
-                    f"discriminator loss : {total_disc_loss / max(i,1)}",
+                    f"discriminator loss : {total_disc_loss / max(i, 1)}",
                 )
 
     return total_vae_loss / len(test_dataloader), total_disc_loss / len(test_dataloader)
@@ -183,8 +192,8 @@ def train_vae(
         results = {
             "vae_loss": [],
             "disc_loss": [],
-            "test_vae_loss" :[],
-            "test_disc_loss" : [],
+            "test_vae_loss": [],
+            "test_disc_loss": [],
         }
 
     model_checkpoint_path = checkpoint_dir / f"{model_name}_latest_epoch.pt"
@@ -195,17 +204,23 @@ def train_vae(
 
         vae.load_state_dict(checkpoint_states["vae_state_dict"])
 
-        discriminator.load_state_dict(checkpoint_states["discriminator_state_dict"])
+        discriminator.load_state_dict(checkpoint_states["disc_state_dict"])
 
         vae_opt.load_state_dict(checkpoint_states["vae_optimizer_state_dict"])
 
         disc_opt.load_state_dict(checkpoint_states["disc_optimizer_state_dict"])
-        
-        if vae_lr_scheduler is not None and checkpoint_states.get("vae_lr_scheduler_state_dict") is not None:
+
+        if (
+            vae_lr_scheduler is not None
+            and checkpoint_states.get("vae_lr_scheduler_state_dict") is not None
+        ):
             vae_lr_scheduler.load_state_dict(
                 checkpoint_states["vae_lr_scheduler_state_dict"]
             )
-        if disc_lr_scheduler is not None and checkpoint_states.get("disc_lr_scheduler_state_dict") is not None:
+        if (
+            disc_lr_scheduler is not None
+            and checkpoint_states.get("disc_lr_scheduler_state_dict") is not None
+        ):
             disc_lr_scheduler.load_state_dict(
                 checkpoint_states["disc_lr_scheduler_state_dict"]
             )
@@ -219,32 +234,51 @@ def train_vae(
         checkpoint_epoch = 0
         global_step = 0
 
+    # instantiate scaler
+
+    scaler = torch.amp.GradScaler("cuda")
+
+    if (
+        model_checkpoint_path.exists()
+        and checkpoint_states.get("scaler_state_dict") is not None
+    ):
+        scaler.load_state_dict(checkpoint_states["scaler_state_dict"])
+
     # training loop
     with alive_bar(epochs - checkpoint_epoch, bar="fish") as bar:
         for epoch in range(checkpoint_epoch, epochs):
-            vae_loss, disc_loss, global_step = train_step(vae = vae, discriminator = discriminator, disc_opt = disc_opt, vae_opt = vae_opt, loss_fn = vae_loss, train_dataloader = train_dataloader, kl_scheduler=kl_annealing_scheduler, step = global_step, device = device)
+            vae_loss_avg, disc_loss, global_step = train_step(
+                vae=vae,
+                discriminator=discriminator,
+                disc_opt=disc_opt,
+                vae_opt=vae_opt,
+                loss_fn=vae_loss,
+                train_dataloader=train_dataloader,
+                kl_scheduler=kl_annealing_scheduler,
+                step=global_step,
+                device=device,
+                scaler=scaler,
+            )
 
-            # generate a few preview images on test set 
+            # generate a few preview images on test set
 
             with torch.inference_mode():
+                plt.figure(figsize=(16, 8))
 
-                plt.figure(figsize = (16,8))
+                vae.eval()
+                x = next(iter(test_dataloader)).to(device)
+                gen_img, _, _ = vae(x)
 
                 for i in range(4):
-                
-                    vae.eval()
-                    x = next(iter(test_dataloader))
-                    gen_img, _, _ = vae(x)
-
                     orig = x[i].cpu().permute(1, 2, 0).squeeze()
                     recon = gen_img[i].cpu().permute(1, 2, 0).squeeze()
 
-                    plt.subplot(4,2,i * 2+1)
+                    plt.subplot(4, 2, i * 2 + 1)
                     plt.imshow(recon)
                     plt.title("generated by vae")
                     plt.axis("off")
 
-                    plt.subplot(4,2,i*2 + 2)
+                    plt.subplot(4, 2, i * 2 + 2)
                     plt.imshow(orig)
                     plt.title("input img")
                     plt.axis("off")
@@ -253,51 +287,66 @@ def train_vae(
                 plt.pause(10)
                 plt.close("all")
 
-            test_vae_loss, test_disc_loss = test_step(vae = vae, discriminator = discriminator, test_dataloader = test_dataloader, loss_fn = vae_loss, kl_scheduler = kl_annealing_scheduler, step = global_step, device = device)
+            test_vae_loss, test_disc_loss = test_step(
+                vae=vae,
+                discriminator=discriminator,
+                test_dataloader=test_dataloader,
+                loss_fn=vae_loss,
+                kl_scheduler=kl_annealing_scheduler,
+                step=global_step,
+                device=device,
+            )
 
-            print(f"epoch         : {epoch} ------------------------------------------\n",
-                  f"train vae_loss: {vae_loss}     | train disc_loss : {disc_loss}\n",  
-                  f"------------------------------------------------------------------\n",
-                  f"test vae_loss : {test_vae_loss} | test disc_loss : {test_disc_loss}"
-                )
-            
-            results["vae_loss"].append(vae_loss)
+            print(
+                f"epoch         : {epoch} ------------------------------------------\n",
+                f"train vae_loss: {vae_loss_avg}     | train disc_loss : {disc_loss}\n",
+                f"------------------------------------------------------------------\n",
+                f"test vae_loss : {test_vae_loss} | test disc_loss : {test_disc_loss}",
+            )
+
+            results["vae_loss"].append(vae_loss_avg)
             results["disc_loss"].append(disc_loss)
             results["test_vae_loss"].append(test_vae_loss)
             results["test_disc_loss"].append(test_disc_loss)
 
-            # scheduler step 
+            # scheduler step
             if vae_lr_scheduler is not None:
                 vae_lr_scheduler.step()
             if disc_lr_scheduler is not None:
                 disc_lr_scheduler.step()
 
-            # checkpointing 
+            # checkpointing
 
             checkpoint_states = {
-                "vae_state_dict" : vae.state_dict(),
-                "disc_state_dict" : discriminator.state_dict(),
-                "vae_optimizer_state_dict" : vae_opt.state_dict(),
-                "disc_optimizer_state_dict" : disc_opt.state_dict(),
-                "vae_lr_scheduler_state_dict" : vae_lr_scheduler.state_dict() if vae_lr_scheduler is not None else None,
-                "disc_lr_scheduler_state_dict" : disc_lr_scheduler.state_dict() if disc_lr_scheduler is not None else None,
-                "global_step" : global_step,
-                "epoch" : epoch,
+                "vae_state_dict": vae.state_dict(),
+                "disc_state_dict": discriminator.state_dict(),
+                "vae_optimizer_state_dict": vae_opt.state_dict(),
+                "disc_optimizer_state_dict": disc_opt.state_dict(),
+                "vae_lr_scheduler_state_dict": vae_lr_scheduler.state_dict()
+                if vae_lr_scheduler is not None
+                else None,
+                "disc_lr_scheduler_state_dict": disc_lr_scheduler.state_dict()
+                if disc_lr_scheduler is not None
+                else None,
+                "global_step": global_step,
+                "epoch": epoch,
+                "scaler_state_dict": scaler.state_dict(),
             }
 
             torch.save(checkpoint_states, model_checkpoint_path)
 
-
             if epoch >= 10:
-
                 save_model_path = checkpoint_dir / "best_models" / model_name
 
                 if not save_model_path.exists():
                     save_model_path.mkdir(parents=True, exist_ok=True)
 
                 if min(results["test_vae_loss"]) == test_vae_loss:
-                    torch.save(checkpoint_states, f"{save_model_path}/{model_name}_epoch_{epoch}_vae_test_loss_{test_vae_loss:.2f}.pt")
-            
+                    torch.save(
+                        checkpoint_states,
+                        f"{save_model_path}/{model_name}_epoch_{epoch}_vae_test_loss_{test_vae_loss:.2f}.pt",
+                    )
+
             with open(result_checkpoint, "wb") as f:
                 pypickle.dump(results, f)
                 print("saved results")
@@ -305,8 +354,3 @@ def train_vae(
             bar()
 
     return results
-
-
-            
-
-
